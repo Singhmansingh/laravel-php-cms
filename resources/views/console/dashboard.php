618696e1a587289a25ace335438907ae996ee8b6
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-04-19 11:22:25 -0400

    Use include for header and footer

--- resources/views/console/dashboard.php
+++ resources/views/console/dashboard.php
@@ -1,33 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>My Portfolio</title>
-
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="/app.css">
-
-        <script src="/app.js"></script>
-
-    </head>
-    <body>
-
-        <header class="w3-padding">
-
-            <h1 class="w3-text-red">Portfolio Console</h1>
-
-            <?php if(Auth::check()): ?>
-                You are logged in as <?= auth()->user()->first ?> <?= auth()->user()->last ?> |
-                <a href="/console/logout">Log Out</a> |
-                <a href="/console/dashboard">Dashboard</a> |
-                <a href="/">Website Home Page</a>
-            <?php else: ?>
-                <a href="/">Return to My Portfolio</a>
-            <?php endif; ?>
-
-        </header>
+<?php
+    include(dirname(__DIR__).'/includes/header.php');
+?>
 
         <hr>
 
@@ -40,10 +13,9 @@
                 <li><a href="/console/experiences/list">Manage Experience</a></li>
                 <li><a href="/console/educations/list">Manage Educations</a></li>
                 <li><a href="/console/skills/list">Manage Skills</a></li>
-                <li><a href="/console/logout">Log Out</a></li>
             </ul>
 
         </section>
-
-    </body>
-</html>
+<?php
+    include( dirname(__DIR__).'/includes/footer.php' );
+?>
